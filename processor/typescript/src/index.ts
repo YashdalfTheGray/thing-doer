f8774bb0eab3caa90321807c974d98bc54846498
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-08-24 00:19:49 -0700

    added a simple server that does nothing for now

--- processor/typescript/src/index.ts
+++ processor/typescript/src/index.ts
@@ -1,3 +1,19 @@
-(async () => {
-  console.log("test");
-})();
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import chalk from 'chalk';
+
+const port = process.env.PORT || process.argv[2] || 8080;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(morgan('common'));
+const apiRouter = express.Router();
+
+app.use('/api', apiRouter);
+
+app.listen(port, () =>
+  // tslint:disable-next-line
+  console.log(`Server running on port ${chalk.green(port)}`)
+);
